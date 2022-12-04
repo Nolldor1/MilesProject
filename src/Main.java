7fa0e4c4252f4ee2d0b06038ded708d2a58b3160
German Gizatullin <snolldors@inbox.ru>
2022-12-04 17:22:32 +0500

    update miles

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        int amount = 2500; //стоимость билета
-        int x = 20; // кол-во рублей для начисления бонуса в соотношении 20:1
-        int miles; // начилсенные мили
-        miles = amount / x;
-        System.out.println("Начисленные мили: " + miles);
+        BonusMilesService service = new BonusMilesService();
+        int price = 10_000;
+        int miles = service.calculate(price);
+        System.out.println(miles);
+
+        // int amount = 2500; //стоимость билета
+        //  int x = 20; // кол-во рублей для начисления бонуса в соотношении 20:1
+        //  int miles; // начилсенные мили
+        //  miles = amount / x;
+        // System.out.println("Начисленные мили: " + miles);
     }
 }
