aa56a6edc9f4bbc93e79f6ac6abea4b0f618d20e
German Gizatullin <snolldors@inbox.ru>
2022-12-10 23:02:18 +0500

    Second commit

--- src/Main.java
+++ src/Main.java
@@ -5,10 +5,5 @@ public class Main {
         int miles = service.calculate(price);
         System.out.println(miles);
 
-        // int amount = 2500; //стоимость билета
-        //  int x = 20; // кол-во рублей для начисления бонуса в соотношении 20:1
-        //  int miles; // начилсенные мили
-        //  miles = amount / x;
-        // System.out.println("Начисленные мили: " + miles);
     }
 }
